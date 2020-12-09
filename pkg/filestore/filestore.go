600bbd6973961c7afb1616eef69f3c2cc9d9dcdd
muthu3107 <muthuofficial.m@gmail.com>
2020-12-09 16:48:35 +0530

    internal package error fix

--- pkg/filestore/filestore.go
+++ pkg/filestore/filestore.go
@@ -17,8 +17,8 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/tus/tusd/internal/uid"
-	"github.com/tus/tusd/pkg/handler"
+	"github.com/muthu3107/tusd/internal/uid"
+	"github.com/muthu3107/tusd/pkg/handler"
 )
 
 var defaultFilePerm = os.FileMode(0664)
